d6c30456be35dad4ef8a69dc6153344d5347d8c4
Adrien <g.adri63@hotmail.fr>
2025-01-21 22:21:46 +0100

    Init repo

---
+++ Backend/IMINBackend.Contracts/Settings/PostgresSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace IMINBackend.Contracts.Settings;
+
+/// <summary>
+/// Define the structure for postgres settings
+/// </summary>
+public class PostgresSettings
+{
+    /// <summary>
+    /// The section name
+    /// </summary>
+    public const string SectionName = "Postgres";
+
+    public string? Host { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? DbName { get; set; }
+    public int? MinPoolSize { get; set; }
+    public int? MaxPoolSize { get; set; }
+    public int? CommandTimeout { get; set; }
+}
---
+++ Backend/IMINBackend.Services/ServicesExtension.cs
@@ -0,0 +1,58 @@
+﻿using IMINBackend.Contracts.Settings;
+using IMINBackend.Services.Repositories.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+#pragma warning disable IDE0130 // Le namespace ne correspond pas à la structure de dossiers
+namespace Microsoft.Extensions.DependencyInjection
+#pragma warning restore IDE0130 // Le namespace ne correspond pas à la structure de dossiers
+{
+    /// <summary>
+    /// Extension of <see cref="IServiceCollection"/>
+    /// </summary>
+    public static class ServicesExtension
+    {
+        /// <summary>
+        /// Add services dedicated to this project
+        /// </summary>
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .Configure<PostgresSettings>(configuration.GetSection(PostgresSettings.SectionName))
+                .ConfigureDbContext(configuration)
+                ;
+            return services;
+        }
+
+        public static IServiceCollection ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var postgresSettings = configuration.GetPostgresSettings();
+            services.AddDbContext<IminDbContext>(option => option.UseNpgsql(postgresSettings.GetConnectionString()));
+            return services;
+        }
+
+        static PostgresSettings GetPostgresSettings(this IConfiguration configuration) => configuration.GetSection("Postgres").Get<PostgresSettings>()!;
+
+        /// <summary>
+        /// Get the connection string from a <see cref="PostgresSettings"/> instance
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        static string GetConnectionString(this PostgresSettings settings)
+        {
+            var builder = new NpgsqlConnectionStringBuilder();
+
+            if (settings.DbName != null) builder.Database = settings.DbName;
+            if (settings.Password != null) builder.Password = settings.Password;
+            if (settings.Username != null) builder.Username = settings.Username;
+            if (settings.Host != null) builder.Host = settings.Host;
+            if (settings.MinPoolSize != null) builder.MinPoolSize = settings.MinPoolSize ?? 0;
+            if (settings.MaxPoolSize != null) builder.MaxPoolSize = settings.MaxPoolSize ?? 0;
+            if (settings.CommandTimeout != null) builder.CommandTimeout = settings.CommandTimeout ?? 0;
+
+            return builder.ToString();
+        }
+
+    }
+}
