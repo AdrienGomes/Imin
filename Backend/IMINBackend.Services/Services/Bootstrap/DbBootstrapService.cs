6994927cece13be55b531096d0b8f6a211b847c3
Adrien <g.adri63@hotmail.fr>
2025-02-02 17:43:54 +0100

    Add EF migration bootstrap service

---
+++ Backend/IMINBackend.Services/Services/Bootstrap/DbBootstrapService.cs
@@ -0,0 +1,42 @@
+﻿using IMINBackend.Contracts.Services;
+using IMINBackend.Services.Repositories.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMINBackend.Services.Services.Bootstrap;
+
+/// <summary>
+/// Service to bootstrap the <see cref="IminDbContext"/>
+/// </summary>
+public class DbBootstrapService(IminDbContext context) : IAppBootstrapService
+{
+
+    /// <inheritdoc/>
+    public async Task DoAsync() => await ApplyEfMigration();
+
+    /// <summary>
+    /// Apply EF migrations
+    /// </summary>
+    /// <remarks>
+    /// This methods only apply migrations, adding migration is done manually by dev. See EF tool add migration doc
+    /// </remarks>
+    async Task ApplyEfMigration()
+    {
+
+        // TODO : should move to Ilogger
+        Console.WriteLine("Applying EF migrations");
+
+        try
+        {
+            await context.Database.MigrateAsync();
+
+            // TODO : should move to Ilogger
+            Console.WriteLine("EF migrations succesfully applied");
+
+        }
+        catch (Exception ex)
+        {
+            // TODO : should move to Ilogger
+            Console.WriteLine($"An exception has occured during Ef migrations : {ex}");
+        }
+    }
+}
--- Backend/IMINBackend.Services/ServicesExtension.cs
+++ Backend/IMINBackend.Services/ServicesExtension.cs
@@ -1,5 +1,7 @@
-﻿using IMINBackend.Contracts.Settings;
+﻿using IMINBackend.Contracts.Services;
+using IMINBackend.Contracts.Settings;
 using IMINBackend.Services.Repositories.Context;
+using IMINBackend.Services.Services.Bootstrap;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
@@ -21,24 +23,47 @@ namespace Microsoft.Extensions.DependencyInjection
             services
                 .Configure<PostgresSettings>(configuration.GetSection(PostgresSettings.SectionName))
                 .ConfigureDbContext(configuration)
+                .AddBootstrapServices(configuration)
                 ;
             return services;
         }
 
-        public static IServiceCollection ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        /// <summary>
+        /// Add <see cref="IAppBootstrapService"/>s to the service container
+        /// </summary>
+        /// <remarks>
+        /// This registration must be done right after app builder creation</remarks>
+        static IServiceCollection AddBootstrapServices(this IServiceCollection services, IConfiguration configuration)
         {
-            var postgresSettings = configuration.GetPostgresSettings();
-            services.AddDbContext<IminDbContext>(option => option.UseNpgsql(postgresSettings.GetConnectionString()));
+            services
+                // db bootstrap serice
+                .AddSingleton<IAppBootstrapService, DbBootstrapService>()
+                ;
             return services;
         }
 
+
+        /// <summary>
+        /// Configure <see cref="IminDbContext"/>
+        /// </summary>
+        static IServiceCollection ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var postgresSettings = configuration.GetPostgresSettings();
+            services.AddDbContext<IminDbContext>(
+                option => option.UseNpgsql(postgresSettings.GetConnectionString()),
+                contextLifetime: ServiceLifetime.Singleton
+                );
+            return services;
+        }
+
+        /// <summary>
+        /// Get postgres settings from configuration
+        /// </summary>
         static PostgresSettings GetPostgresSettings(this IConfiguration configuration) => configuration.GetSection("Postgres").Get<PostgresSettings>()!;
 
         /// <summary>
         /// Get the connection string from a <see cref="PostgresSettings"/> instance
         /// </summary>
-        /// <param name="settings"></param>
-        /// <returns></returns>
         static string GetConnectionString(this PostgresSettings settings)
         {
             var builder = new NpgsqlConnectionStringBuilder();
@@ -47,6 +72,7 @@ namespace Microsoft.Extensions.DependencyInjection
             if (settings.Password != null) builder.Password = settings.Password;
             if (settings.Username != null) builder.Username = settings.Username;
             if (settings.Host != null) builder.Host = settings.Host;
+            builder.Port = 5432;
             if (settings.MinPoolSize != null) builder.MinPoolSize = settings.MinPoolSize ?? 0;
             if (settings.MaxPoolSize != null) builder.MaxPoolSize = settings.MaxPoolSize ?? 0;
             if (settings.CommandTimeout != null) builder.CommandTimeout = settings.CommandTimeout ?? 0;
