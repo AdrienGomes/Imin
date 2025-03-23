46c0f1c3cffb0603e371d09e96200b4ccf46f20b
Adrien <g.adri63@hotmail.fr>
2025-03-23 20:33:16 +0100

    Add Volunteer model

---
+++ Backend/IMINBackend.Services/Model/Association/PoleMembership.cs
@@ -0,0 +1,33 @@
+﻿using IMINBackend.Services.Contract.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IMINBackend.Services.Model.Association;
+
+/// <summary> Represent a pole membership for a volunteer </summary>
+public class PoleMembership : BaseIdendifiedElement
+{
+    [Required]
+    /// <summary> The id of the pole </summary>
+    public required string PoleId { get; set; }
+
+    [ForeignKey(nameof(PoleId))]
+    /// <summary> The pole </summary>
+    public required Pole Pole { get; set; }
+
+    [Required]
+    /// <summary> The id of the volunteer </summary>
+    public required string VolunteerId { get; set; }
+
+    [ForeignKey(nameof(VolunteerId))]
+    /// <summary> The volunteer </summary>
+    public required Volunteer Volunteer { get; set; }
+
+    [Required]
+    /// <summary> The priority of the pole for the volunteer </summary>
+    public required string Priority { get; set; }
+
+    [EnumDataType(typeof(PoleStatus))]
+    [Column(TypeName = "varchar(30)")]
+    public required PoleStatus Status { get; set; }
+}
